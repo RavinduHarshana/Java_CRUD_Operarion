1329a7ae2fdccba04778049ebd08135749209dbd
RavinduHarshana <ravinduharshana2001@gmail.com>
2024-04-05 13:12:40 +0530

    Initial COmmit

---
+++ src/Create.java
@@ -0,0 +1,29 @@
+import  java.sql.*;
+public class Create {
+    public static void main(String[] args) {
+        String url="jdbc:mysql://localhost:3306/java_test";
+        String user="root";
+        String password="1234";
+        String Query="INSERT INTO user values(1,'jhone','er','Mawanella');";
+
+        Connection connection=null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection =DriverManager.getConnection(url,user,password);
+            Statement statement=connection.createStatement();
+            statement.executeUpdate(Query);
+
+        }catch (Exception e){
+            System.out.println("Error");
+        }
+        finally {
+            try {
+                if (connection!=null)
+                    connection.close();
+            }catch (SQLException e){
+                System.out.println("Closing Error");
+            }
+        }
+    }
+}
